1f61fc973d8fdcb896685810bdf93952882cbb1d
RyanJellesed <ryan_jellesed@hotmail.com>
2016-02-16 14:01:42 -0700

    ..

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/animals');
 
+var Bear = require('./models/bears');
 
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
@@ -13,11 +14,46 @@ var port = process.env.PORT || 8080;
 
 var router = express.Router();
 
+router.use(function(req, res, next) {
+	console.log('something is happening!');
+	next();
+});
+
 router.get('/', function(req, res) {
 	res.json({ message: 'hooray! welcome to our api!' });
 });
 
+router.route('/bears')
+	.post(function(req, res) {
+
+		var bear = new Bear();
+		bear.name = req.body.name;
+		bear.age = req.body.age;
+		bear.gender = req.body.gender;
+
+		bear.save(function(err, bear) {
+			if(err) {
+				//do something
+				console.log(err);
+			}else{
+				//do something
+				res.json(bear);
+			}
+		});
+	});
+
 app.use('/api', router);
+
+router.route('/bears').post(function (req, res) {
+	res.json({ message: 'A post occurred'});
+});
+
+router.route('/bears').get(function(req, res) {
+	res.json({ message: 'A get occured'});
+});
+
+
+
 
 app.listen(port);
 console.log('Magic happens on port ' + port);
