d4da7fb236d46471fe1893e0b9c87478b286a777
RyanJellesed <ryan_jellesed@hotmail.com>
2016-02-17 12:11:36 -0700

    added index.ejs file wed, feb 17th

--- routes/bears.js
+++ routes/bears.js
@@ -1,36 +1,9 @@
 var express = require('express');
-var app     = express();
-var bodyParser = require('body-parser');
-
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/animals');
-
-var bearRouter = require ('./routes/bears');
-
-var Bear = require('./models/bears');
-
-app.use(bodyParser.urlencoded({ extended: true}));
-app.use(bodyParser.json());
-
-app.set('view engine', 'ejs');
-
-app.get('/', function(req, res) {
-	res.render('index', {title: 'Hipster Ipsum!'})
-	res.render('index', {date: newDate()})
-})
-
-var port = process.env.PORT || 8080;
-
 var router = express.Router();
 
-router.use(function(req, res, next) {
-	console.log('something is happening!');
-	next();
-});
+var Bear = require('../models/bears')
 
-router.get('/', function(req, res) {
-	res.json({ message: 'hooray! welcome to our api!' });
-});
+
 
 router.route('/bears')
 	.post(function(req, res) {
@@ -104,20 +77,13 @@ router.route('/bears/:bear_id')
 	});
 
 
-app.use('/api', bearRouter);
-
-
-
-router.route('/bears').post(function (req, res) {
-	res.json({ message: 'A post occurred'});
-});
-
-router.route('/bears').get(function(req, res) {
-	res.json({ message: 'A get occured'});
-});
 
 
 
 
-app.listen(port);
-console.log('Magic happens on port ' + port);
+
+
+
+
+module.exports = router;
+
