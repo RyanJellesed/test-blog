ae974f79f27e2f8e50dbe6c9d0f2caae4ba216a8
RyanJellesed <ryan_jellesed@hotmail.com>
2016-02-18 14:12:04 -0700

    I'm so lost :-)

--- public/app.js
+++ public/app.js
@@ -1,14 +1,24 @@
 var deleteBear = function () {
 	var id = $(event.target).closest('tr')attr('id');
 
-	aler('The id of this bear is: ' + id);
+	var id = $(event.target).closest('tr').attr('id');
+	var bear = $(event.target).closest('tr');
+
+	$.ajax({
+		url: '/api/bears/' + id,
+		method: 'DELETE',
+	}).done(function () {
+		console.log('bear deleted!')
+		bear.remove();
+	})
 
 }
 
-
 $('.deleteBear').on('click', deleteBear);
 
-
+var addBear = function(event){
+	
+}
 
 
 
