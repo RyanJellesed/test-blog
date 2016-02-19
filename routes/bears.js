80ff6a053d3131682b0085fc14866045dd46b763
RyanJellesed <ryan_jellesed@hotmail.com>
2016-02-19 12:11:37 -0700

    added more notes from the code walkthrough, bears.js file

--- routes/bears.js
+++ routes/bears.js
@@ -1,10 +1,12 @@
-var express = require('express');
+var express = require('express'); // requires express 
 
-var router = express.Router();
+// express.Router() is a method on express
+var router = express.Router(); // we are getting a new object called router
 
-var Bear = require('../models/bears')
+var Bear = require('../models/bears') // we are requiring our Schema from ../models/bears file
 
 
+// this creates 
 router.use(function(req, res, next) {
 	console.log('something is happening!');
 	next();
@@ -12,28 +14,30 @@ router.use(function(req, res, next) {
 
 
 
-router.route('/bears')
-	.post(function(req, res) {
+router.route('/bears')  // ?????
+	.post(function(req, res) {  // 
 
-		var bear = new Bear();
+		var bear = new Bear(); // calling schema and constructing a new bear
 
-		bear.name = req.body.name;
-		bear.age = req.body.age;
-		bear.gender = req.body.gender;
+		bear.name = req.body.name; // pulling the data from the request (can come from our form or Postman or somewhere else)
+		bear.age = req.body.age; // same as above
+		bear.gender = req.body.gender; // same as above
 
-		bear.save(function(err, bear) {
+		// .save is a model of mongoose.model.save
+		bear.save(function(err, bear) { //creating a new intance of bear and 
 			if(err) {
 				//do something
 				res.send(err)
 			} else {
 				//do something
-				res.json(bear);
+				res.json(bear); // res (response) with json (javascript object notation) of the variable bear, 
+				                //which is an instance of the New Bear() object constructor function above (line 20) which is located in our bears.js file
 			}
 		})
 	})
 
 	.get(function(req, res) {
-		Bear.find(function(err, bears) {
+		Bear.find(function(err, bears) { // looking for the collection of all our bear models. Looking for all the objects of bear.
 			if(err) {
 				return next(err);
 			} else {
@@ -42,14 +46,19 @@ router.route('/bears')
 		})
 	});
 
-router.route('/bears/:bear_id')
+router.route('/bears/:bear_id')  // looking for bears by id in the /bears folder
+								 // could be '/bears/:bear_id/:name' if we also wanted to find name
+								 // the : tells us it's a param
 	.get(function(req, res) {
-		Bear.findById(req.params.bear_id, function (err, bear) {
-			// body...
+		// Bear.findById is another mongoose method
+		// req.params.bear_id is looking at what we put up in the url
+		// req.params.
+		Bear.findById(req.params.bear_id, function (err, bear) { // function (err, bear) { .. } is the callback and lets us know if we err or don't 
+															     // (which is the var bear we set on line 20)
 			if(err) {
-				console.log(err);
+				console.log(err); // if there was an err it will be console logged
 			} else {
-				res.json(bear);
+				res.json(bear); // if no error. This is the same as line 33 above. We will get a response. 
 			}
 		})
 	})
@@ -73,15 +82,16 @@ router.route('/bears/:bear_id')
 		})
 	})
 	.delete(function(req, res) {
+		// ask about the leading _id: (the underscore)
 		Bear.remove({_id: req.params.bear_id}, function(err, bear) {
 			if (err) {
 				console.log(err)
 			} else {
-				res.json({title: 'bear was successfully deleted!'})
+				res.json({title: 'bear was successfully deleted!'}) // same concept as line 33
 			}
 		})
 	});
 
 
-module.exports = router;
+module.exports = router; // exports the router
 
